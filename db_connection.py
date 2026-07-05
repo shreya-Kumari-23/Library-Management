05b6dc6f75b19c4ee84b7f744187dbaf27478165
shreya-Kumari-23 <shreya01g@gmail.com>
2026-07-05 17:16:09 +0530

    Remove old Python files from root after moving to src

--- db_connection.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector as m
-
-def get_db_connection():
-    return m.connect(
-        host="localhost",
-        user="root",
-        passwd="root",
-        database="YOUR_LIBRARY"
-    )
